cc2c38b98017877c2170d3cdf6fe32a1e7dfc7fc
john <jiama843@gmail.com>
2019-08-11 18:03:43 -0400

    Fixed product description

--- root/components/screen/description.js
+++ root/components/screen/description.js
@@ -10,10 +10,6 @@ function Description(props) {
                 <p class="black-70 pt4-m pt6-l f4 f4-m f3-l fw4 lh-copy">
                     However, many people are unaware of light therapy, and existing products on the market do not offer a portable and discreet method of treatment.
                 </p>
-        
-                <p class="black-90 pt4-m pt6-l f4 f4-m f3-l fw4 lh-copy">
-                    Using innovative design and state of the art technology, our product, Lumos glasses, aims to solve these problems by being the most fashionable, and competitive light therapy glasses on the market.
-                </p>
             </div>
             
         </div>
@@ -31,11 +27,7 @@ function Description(props) {
     return (
         <div
             class="description mt-5 center contain container pa3 ph5-ns flex flex-wrap justify-center pt7">
-            <h1
-                class="pt7 f2 f1-l normal pv3"
-            >
-                Our Product
-            </h1>   
+ 
             <SplitComponents
                 left={descDiv}
                 right={imageDiv}
--- root/components/screen/images.js
+++ root/components/screen/images.js
@@ -1,21 +1,40 @@
 function ImageCol(props) {
     var imageStyle = {
-        backgroundPosition: "center center",
         display: "block",
-        maxWidth: "80%",
-        maxHeight: "80%",
+        maxWidth: "50%",
+        maxHeight: "50%",
         width: "auto",
         height: "auto",
+        borderRadius: "5%",
         zIndex: "0"
     };
-    
+
+    var textStyle = {
+        textAlign: "left",
+        marginLeft: "20%",
+        marginRight: "20%"
+    };
+
     return (
-        <div class="vh-100 mt-5 center contain container pt5">
-            <img
-                class="col-xs-12 col-sm-6 col-md-6 col-lg-6 center"
-                style={imageStyle}
-                src="common/images/lumos-glasses.jpg"
-            />
+        <div class="mt-5 center justify-center flex flex-column contain container pt7">
+            <h1
+                class="f2 f1-l normal center pt5"
+            >
+                Our Product
+            </h1>  
+            <p 
+                class="black-90 f4 f4-m f3-l fw4 lh-copy"
+                style={textStyle}
+            >
+                Using innovative design and state of the art technology, our product, Lumos glasses, aims to solve these problems by being the most fashionable, and competitive light therapy glasses on the market.
+            </p>
+            <div class="flex flex-column pv5">
+                <img
+                    class="center"
+                    style={imageStyle}
+                    src="common/images/lumos-glasses.jpg"
+                />
+            </div>
         </div>
     );
 }
