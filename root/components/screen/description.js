d19403a5b5fe4abf0a3f44928deae686619f950d
Taehyun Lee <taehyunlee4482@gmail.com>
2019-08-11 14:27:03 -0400

    update design of front page (#6)
    * update design of front page
    
    * Added to description and added header

--- root/components/screen/description.js
+++ root/components/screen/description.js
@@ -1,23 +1,52 @@
 function Description(props) {
-    var textStyles = {
-        position: "relative",
-        display: "block",
-        line_height: "1.2"
-    };
     
-    return (
-        <div
-            class="vh-100 mt-5 center contain container">
-            <div class="mw5 mw6-m mw8-l center flex flex-column">
-                <h1
-                    class="pt7 f2 f1-l tracked center"
-                >
-                    Our Product
-                </h1>
-                <p class="f4 f4-m f3-l fw4" style={textStyles}>
-                    Using light that mimics the brightness of the natural outdoors, light therapy can help treat the winter blues as well as improve sleep quality for people with sleep schedule problems. However, many people are unaware of light therapy and existing products on the market do not offer a portable and discreet method of treatment. Our product, Lumos glasses, is a pair of light therapy glasses that aims to solve these problems with innovative technology. Through creative design, we have made Lumos glasses to be the most effective, fashionable, and affordable light therapy glasses on the market.
+    var descDiv = (
+        <div class="flex flex-wrap items-center h-100 pt3">
+            <div>
+                <p class="black-50 f4 f4-m f3-l fw4 lh-copy">
+                    Using light that mimics the brightness of the natural outdoors, light therapy can help treat the winter blues as well as improve sleep quality for people with sleep schedule problems.
+                </p>
+
+                <p class="black-70 pt4-m pt6-l f4 f4-m f3-l fw4 lh-copy">
+                    However, many people are unaware of light therapy, and existing products on the market do not offer a portable and discreet method of treatment.
+                </p>
+        
+                <p class="black-90 pt4-m pt6-l f4 f4-m f3-l fw4 lh-copy">
+                    Using innovative design and state of the art technology, our product, Lumos glasses, aims to solve these problems by being the most fashionable, and competitive light therapy glasses on the market.
                 </p>
             </div>
+            
+        </div>
+    );
+
+    var imageDiv = (
+        <div class="w-100 pt3-ns flex flex-wrap justify-center">
+            {competitors.map(competitor => (
+                <img src={competitor} class="mw5 ph3 pv1 ph3 pv3 pv4">
+                </img>
+            ))}
+        </div>
+    );
+
+    return (
+        <div
+            class="description mt-5 center contain container pa3 ph5-ns flex flex-wrap justify-center pt7">
+            <h1
+                class="pt7 f2 f1-l normal pv3"
+            >
+                Our Product
+            </h1>   
+            <SplitComponents
+                left={descDiv}
+                right={imageDiv}
+            />
+
         </div>
     );
 }
+
+/*
+<p class="ph5 f4 f4-m f3-l fw4 lh-copy">
+                    Our product, Lumos glasses, is a pair of light therapy glasses that aims to solve these problems with innovative technology. Through creative design, we have made Lumos glasses to be the most effective, fashionable, and affordable light therapy glasses on the market.
+                </p>
+*/
--- root/components/screen/vision.js
+++ root/components/screen/vision.js
@@ -1,21 +1,23 @@
 function Vision(props) {
-    var headerStyles = {
-        position: "relative",
-        left: "50%"
-    };
+
+    var visionDiv = (
+        <div class="mw5 mw6-m mw8-l center flex flex-column tr">
+            <h1
+                class="pt7 f2 f1-l normal"
+            >
+                Our Vision
+                </h1>
+            <h2 class="f4 f4-m f3-l fw4 lh-copy">To help people live healthier lives through technology and design</h2>
+        </div>
+    );
 
     return (
         <div
-            class="vh-100 mt-5 center contain container">
-            <div class="mw5 mw6-m mw8-l center flex flex-column">
-                <h1
-                    class="pt7 f2 f1-l tracked"
-                    style={headerStyles}
-                >
-                    Our Vision
-                </h1>
-                <h2 class="f4 f4-m f3-l fw4 center">To help people live healthier lives through technology and design</h2>
-            </div>
+            class="vh-100 mt-5 center contain container pa3 pa5-ns">
+            <SplitComponents
+                right={visionDiv}
+            />
+
         </div>
         
     );
